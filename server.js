7d481e95c261c7eea2255a0bf2a9b73653dfcd36
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2025-03-24 10:14:07 +0100

    boilerplate

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASS,
+    database: process.env.DB
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error("Database connection failed:", err);
+        return;
+    }
+    console.log("Connected to database!");
+});
+
+const PORT = process.env.PORT || 13001;
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
