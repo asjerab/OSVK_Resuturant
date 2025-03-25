46d2a7475933b00d13abb9ec00376a0ad4002af2
asjerab <asjer.contact@gmail.com>
2025-03-25 09:32:56 +0100

    front end design start

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
 require("dotenv").config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const mysql = require('mysql2');
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static("public"));
 
 const connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASS,
-    database: process.env.DB
+  host: process.env.HOST,
+  user: process.env.USERTEST,
+  password: process.env.PASS,
+  database: process.env.DB,
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.error("Database connection failed:", err);
-        return;
-    }
-    console.log("Connected to database!");
+  if (err) {
+    console.error("Database connection failed:", err);
+    return;
+  }
+  console.log("Connected to database!");
 });
 const PORT = process.env.PORT || 13001;
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
+
+app.use(express.static("public"));
 
